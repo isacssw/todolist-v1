ce8370e0f4d22b67d091f2a520804af27f873f57
Isac X <isacssw@gmail.com>
2021-03-04 23:44:00 +0000

    changing css

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
+const items = ["Buy Food","Cook Food","Eat the Food"];
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
 
-    var today = new Date();
-    
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    day = today.toLocaleDateString("en-Us", options);
-
-    res.render("list", {kindOfDay: day});
-
-
+  const day = date.getDate();
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
-    const newItem = req.body.nItem;
-    console.log(newItem);
+    const item = req.body.newItem;
+
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
