55584bd2299645ec5bb379bb0749975f94fb64bc
Isac X <isacssw@gmail.com>
2021-03-04 21:44:40 +0000

    updating code

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("Hello");
+
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    day = today.toLocaleDateString("en-Us", options);
+
+    res.render("list", {kindOfDay: day});
+
+
+});
+
+app.post("/", function(req, res) {
+    const newItem = req.body.nItem;
+    console.log(newItem);
 });
 
 app.listen(3000, function(){
